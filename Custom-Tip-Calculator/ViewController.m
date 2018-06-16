00fef7d7d7ef5b8d7ed8c67b3cbf836c98f2bf61
Erik Goossens <erikmgoossens@gmail.com>
2018-06-16 14:56:04 -0400

    First commit with base tip calculating functionality

--- Custom-Tip-Calculator/ViewController.m
+++ Custom-Tip-Calculator/ViewController.m
@@ -8,22 +8,64 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentageTextField;
 
 @end
 
 @implementation ViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.tipAmountLabel.text = @"";
+    self.billAmountTextField.delegate = self;
 }
 
+- (IBAction)calculateTip:(id)sender {
+    [self.billAmountTextField resignFirstResponder];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    float tipPercentValue = (self.tipPercentageTextField.text.floatValue)/100.0;
+    
+    if ([self.tipPercentageTextField.text isEqualToString:@""]) {
+        tipPercentValue = .15;
+    }
+    NSString *billAmount = self.billAmountTextField.text;
+    float billValue = billAmount.floatValue;
+    float tipAmount = billValue * tipPercentValue;
+    float total = tipAmount + billValue;
+    NSString *result = @(total).stringValue;
+    self.tipAmountLabel.text = result;
+    
+    
+    /* 1. take textfield input text and convert to strong
+     2. convert string to int value
+     3/ take into account the custom tip percentage
+     4. update label with calculated bill amount + tip
+     
+     
+     */
 }
 
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
