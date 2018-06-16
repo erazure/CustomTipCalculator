573277efff9ee416c1f58c952233a03539d86b9d
Erik Goossens <erikmgoossens@gmail.com>
2018-06-16 13:04:53 -0400

    Initial Commit

---
+++ Custom-Tip-Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Custom-Tip-Calculator
+//
+//  Created by Erik Goossens on 2018-06-16.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Custom-Tip-Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Custom-Tip-Calculator
+//
+//  Created by Erik Goossens on 2018-06-16.
+//  Copyright © 2018 Erik Goossens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
